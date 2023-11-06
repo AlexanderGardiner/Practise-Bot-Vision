47cb4c492723f336513a1dfc1d6b49b7c198670d
Aayush Kokate <aayushext@outlook.com>
2023-11-05 16:02:30 -0800

    cleaned up student working code

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,44 +1,45 @@
-package frc.robot;
+package frc.robot.subsystems;
+
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-
 public class Vision {
     public static Vision INSTANCE;
 
-    public static Vision getInstance(){
-        if (INSTANCE == null){
+    public static Vision getInstance() {
+        if (INSTANCE == null) {
             INSTANCE = new Vision();
-        }   
-        
+        }
+
         return INSTANCE;
     }
 
     private final PhotonCamera tagCam = new PhotonCamera("Microsoft_LifeCam_HD-3000");
 
-    private PhotonPipelineResult pipeLast;
+    private PhotonPipelineResult currentResults;
 
     public void periodic() {
-        pipeLast = tagCam.getLatestResult();
+        if (tagCam.getLatestResult().hasTargets()) {
+            currentResults = tagCam.getLatestResult();
+        }
         System.out.println("Best Target:" + this.getBestTarget());
     }
 
-    public PhotonTrackedTarget getBestTarget(){
-        if(pipeLast.hasTargets()){
-            return pipeLast.getBestTarget();
-        }
-        else{
+    public PhotonTrackedTarget getBestTarget() {
+        if (currentResults.hasTargets()) {
+            return currentResults.getBestTarget();
+        } else {
             return null;
         }
     }
-    public boolean pipeLastHasTarget(){
-        if(pipeLast == null){
+
+    public boolean camHasTarget() {
+        if (currentResults == null) {
             return false;
-        }
-        else{
+        } else {
             return true;
         }
     }
-    
+
 }
